546e3f166749dc0a81c350aaf387901b35da0841
Arina76576 <sklyarova2011arina@gmail.com>
2025-03-11 23:28:27 +0300

    Дз1

--- HW1.py
+++ HW1.py
@@ -1,14 +1,18 @@
 print("\033[36mВизначення оцінки")
 print('='*17,'\033[36m\n')
-mark=int(input('Ваша оцінка:'))
-print('\033[33m\033[2m')
-if mark >=0 and mark <=49:
-    print('\033[31mНезадовільно')
-elif 50<=mark<=69:
-    print('\033[35mЗадовільно')
-elif 70<=mark<=89:
-    print('\033[33mДобре')
-elif 90<=mark<=100:
-    print('\033[32mВідмінно')
-else:
-    print('\033[31mПомилка у введені оцінки')
+ans='1'
+while ans=='1':
+# for i in range(3):
+    mark=int(input('\033[36m\033[2mВаша оцінка:'))
+    if mark >=0 and mark <=49:
+        print('\033[31mНезадовільно\n')
+    elif 50<=mark<=69:
+        print('\033[35mЗадовільно\n')
+    elif 70<=mark<=89:
+        print('\033[33mДобре\n')
+    elif 90<=mark<=100:
+        print('\033[32mВідмінно\n')
+    else:
+        print('\033[31mПомилка у введені оцінки')
+    ans=input('\033[36mПродовжити (1-так | 2-ні)')
+# print('========= END ==========')
