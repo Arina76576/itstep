402e019e0d9a9a56936b20012bdcc0b8235f845e
Arina76576 <sklyarova2011arina@gmail.com>
2025-03-22 03:34:17 +0300

    Life students

--- Student CW3.py
+++ Student CW3.py
@@ -2,20 +2,35 @@ import random as r
 class Student:
     def __init__(self, name="Яна"):
         self.name=name
+        self.money=r.randint(100,1000)
         self.happy=r.randint(50,100)
         self.progress=r.randint(1,12)
         self.isStudy=True
+
     def study(self):
         print('Час для навчання')
         self.happy-=r.randint(10,50)
-        self.progress+=r.randint(1,3)
+        self.progress+=r.randint(1,5)
+        self.money-=r.randint(100,1000)
+
     def chill(self):
         print('Час для відпочинку')
         self.happy += r.randint(10, 50)
         self.progress -= r.randint(2, 5)
+        self.money -= r.randint(100, 1000)
+
+    def work(self):
+        print('Треба працювати!')
+        self.happy -= r.randint(10, 50)
+        self.progress += r.randint(1, 12)
+        self.money += r.randint(100, 1000)
+
     def sleep(self):
         print('Час для сну')
         self.happy+=r.randint(1,20)
+        self.progress-=r.randint(1,5)
+        self.money-=r.randint(100,1000)
+
     def isLife(self):
         if self.progress>6:
             print('Все добре з навчанням!',end='')
@@ -28,21 +43,40 @@ class Student:
         else:
             print('Тебе відрахували із закладу')
             self.isStudy=False
+
+    def isWork(self):
+        if self.money>1000:
+            print('Грошей достатньо!',end='')
+            if 300<=self.progress<500:
+                print('Треба більше працювати')
+            else:
+                print('Грошей хватає!')
+        elif 100<=self.progress<=300:
+            print('Не вистачає грошей')
+        else:
+            print('У тебе немає грошей')
+            self.isStudy=False
+
     def everyday(self):
         print('Рівень щастя:',self.happy)
-        print('Прогрес навчання',self.progress)
+        print('Прогрес навчання:',self.progress)
+        print('Кількість грошей:',self.money)
+
     def studyLife(self,day):
         day='\n\033[36mДень №'+str(day)+'\033[0m'
         print(day)
-        res=r.randint(1,3)
+        res=r.randint(1,4)
         if res==1:
             self.chill()
         elif res==2:
             self.sleep()
-        else:
+        elif res==3:
             self.study()
-        self.everyday()
-        self.isLife()
+        else:
+            self.work()
+            self.everyday()
+            self.isLife()
+            self.isWork()
 
 st1=Student()
 print('\033[46mЖиття студента:', st1.name,'\033[0m')
@@ -53,9 +87,9 @@ for k in range(1,8):
     st1.studyLife(k)
 print()
 st2=Student('Саша')
-print('\033[46mЖиття студента:', st1.name,'\033[0m')
+print('\033[46mЖиття студента:', st2.name,'\033[0m')
 # print(st1.progress)
 for k in range(1,8):
     if st2.isStudy==False:
         break
-    st1.studyLife(k)
+    st2.studyLife(k)
